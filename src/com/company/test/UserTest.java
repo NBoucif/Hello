befa931846db33be1115c448ab3a5141660b3e98
boucin <Nordyboucif25@gmail.com>
2021-12-02 09:02:29 +0100

    Init Testing

---
+++ src/com/company/test/UserTest.java
@@ -0,0 +1,13 @@
+package com.company.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+}
